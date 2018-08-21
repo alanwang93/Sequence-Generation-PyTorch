d0de68323b52b238072c5aa26a91e1616020c027
alanwang93 <yifanwang1993@gmail.com>
2018-08-21 17:54:54 +0800

    init

---
+++ process.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import argparse
+import os
+
+from multiprocessing import Process
+import data_config
+from utils.vocab import Vocab
+
+def main(args):
+    assert args.config is not None
+    dc = getattr(data_config, args.config)()
+
+    if args.vocab:
+        vocab = Vocab(data_config=dc)
+        vocab._init_vocab()
+        for src_file, tgt_file in zip(dc.train_src, dc.train_tgt):
+            print("Build vocab from {0}, {1}".format(src_file, tgt_file))
+            corpus = dc.build_corpus(src_file, tgt_file)
+            # Build Vocabualry
+            vocab.update(corpus)
+        vocab._finish()
+    else:
+        vocab = Vocab(data_config=dc)
+        vocab.build(rebuild=False)
+
+    vocab.summary()
+
+    if args.extract:
+        pass
+        #print("Extracting features")
+        #file_list = dc.train_list + dc.valid_list + dc.test_list
+        #processes = [Process(target=dc.extract, args=(f, vocab)) for f in file_list]
+        #for p in processes:
+        #    p.start()
+        #for p in processes:
+        #    p.join()
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default=None, help='data config name')
+    parser.add_argument('--vocab', dest='vocab', action='store_true', \
+            help='(Re)build vocabulary')
+    # parser.add_argument('--embed', dest='embed', action='store_true')
+    parser.add_argument('--extract', dest='extract', action='store_true')
+    args = parser.parse_args()
+    main(args)
+
+
+
+
