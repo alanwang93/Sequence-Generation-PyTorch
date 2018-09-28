1b9a96b1a55cf308a9ed990725e8dc00fbeaa26f
alanwang93 <yifanwang1993@gmail.com>
2018-09-28 21:47:16 +0800

    small changes

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 # Sequence to sequence 
-=======
-# Sequence Generation Models
-
->>>>>>> a5cd95416132e86d3f6f39a568716c1b68d63d2d
--- s2s/datasets.py
+++ s2s/datasets.py
@@ -3,13 +3,15 @@
 
 
 import os
-from nltk import word_tokenize as tokenize
-import numpy as np
 
 class DataConfig:
-
     def __init__(self):
-        self.root = '/home/yifan/workspace/seqgen'
+        # default data root
+        self.root = None
+        
+        # suffix
+        self.src = 'src'
+        self.tgt = 'tgt'
         
         self.unk_token = '<UNK>'
         self.pad_token = '<PAD>'
@@ -22,15 +24,14 @@ class Gigawords(DataConfig):
     def __init__(self):
         super().__init__()
         self.name = 'Gigawords'
-        self.path = os.path.join(self.root, 'data/giga')
-        self.train_src = [os.path.join(self.path, 'train.article.txt')]
+        self.path = os.path.join(self.root, 'giga')
+
+        self.train_prefix = [os.path.join(self.path, 'train.article.txt')]
         self.train_tgt = [os.path.join(self.path, 'train.title.txt')]
-        self.valid_src = [os.path.join(self.path, 'valid.article.filter.txt')]
-        self.valid_tgt = [os.path.join(self.path, 'valid.title.filter.txt')]
+        self.valid_src = [os.path.join(self.path, 'valid.article.txt')]
+        self.valid_tgt = [os.path.join(self.path, 'valid.title.txt')]
         self.test_src = [os.path.join(self.path, 'test.article.txt')]
         self.test_tgt = [os.path.join(self.path, 'test.title.txt')]
-
-        self.feats = ['src', 'tgt', 'len_src', 'len_tgt']
 
         self.max_src_len = 100
         self.max_tgt_len = 20
@@ -38,34 +39,9 @@ class Gigawords(DataConfig):
         self.max_vocab = 200000
         self.min_freq = 3
     
-    def build_data(self, src_file, tgt_file, vocab):
-        with open(src_file, 'r') as s, open(tgt_file, 'r') as t:
-            srcs = [] 
-            tgts = []
-            lens_src = []
-            lens_tgt = []
-            #strs_src = []
-            #strs_tgt = []
-
-            for src, tgt in zip(s, t):
-                str_src = src.strip().split(' ')
-                str_tgt = tgt.strip().split(' ')
-                len_src = len(str_src)
-                len_tgt = len(str_tgt)
-                src = vocab.toi(str_src)
-                tgt = vocab.toi(str_tgt)
-
-                srcs.append(src)
-                tgts.append(tgt)
-                lens_src.append([len_src])
-                lens_tgt.append([len_tgt])
-        
-        np.save(os.path.join(path, 'src.npy'), src)
-        np.save(os.path.join(path, 'tgt.npy'), tgt)
-        np.save(os.path.join(path, 'len_src.npy'), len_src)
-        np.save(os.path.join(path, 'len_tgt.npy'), len_tgt)
-
-
+    def build_dataset(self, src_file, tgt_file, vocab):
+        class GigawordsDataset(Dataset):
+    
 
 
 
