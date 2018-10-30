eba0a0b4ce4ca6c4ab1d4d7cac9599ddba08676c
alanwang93 <yifanwang1993@gmail.com>
2018-10-30 21:14:33 +0800

    summary

--- s2s/train.py
+++ s2s/train.py
@@ -15,6 +15,7 @@ import s2s.models as models
 from s2s.utils.dataloader import build_dataloaders
 from s2s.utils.vocab import Vocab
 from s2s.utils.utils import update_config, init_logging, to
+from s2s.utils.summary import Summary
 
 def train(args):
 
@@ -70,6 +71,7 @@ def train(args):
     cf.tgt_vocab_size = len(tgt_vocab)
 
     train, dev, test = build_dataloaders(cf, src_vocab, tgt_vocab)
+    n_train, n_dev, n_test = map(len, [train, dev, test])
 
     # model
     model = getattr(models, cf.model)(cf, src_vocab, tgt_vocab)
@@ -86,27 +88,42 @@ def train(args):
         best_valid_metric = checkpoint['best_valid_metric']
         best_test_metric = checkpoint['best_test_metric']
         best_step = checkpoint['best_step']
+        restart = False
     else:
         step = 0
         best_valid_metric = cf.init_metric
         best_test_metric = cf.init_metric
         best_step = 0
-
+        restart = True
+    
+    # load summary
+    train_summ = Summary(model_path, prefix='train', restart=restart)
+    dev_summ = Summary(model_path, prefix='dev', restart=restart)
+    test_summ = Summary(model_path, prefix='test', restart=restart)
     
     stop_train = False
     train_loss = 0
     n_train_batch = 0
-    for epoch in itertools.count():
+
+    for _ in itertools.count():
         for i, train_batch in enumerate(train):
+
             # train
             train_batch = to(train_batch, device) 
             train_loss_, n_train_batch_ = model.train_step(train_batch)
             train_loss += train_loss_*n_train_batch_
             n_train_batch += n_train_batch_
             step += 1
+            epoch = step//n_train
 
             if step % cf.log_freq == 0:
-                logger.info('[Train] Step {0}, Loss: {1:.5f}'.format(step, train_loss/n_train_batch))
+                # train summary
+                train_metrics = {'loss':train_loss}
+                train_summ.write(step, train_metrics)
+
+                train_loss /= n_train_batch
+                logger.info('[Train] Step {0}, Loss: {1:.5f}'.format(step, train_loss))
+
                 train_loss = 0
                 n_train_batch = 0
 
@@ -114,18 +131,45 @@ def train(args):
             # preds = model.greedy_decode(batch)
 
             # eval on dev set
-            # dev_loss_all = 0
             if step % cf.eval_freq == 0:
                 dev_loss = 0
                 n_dev_batch = 0
+
                 for dev_batch in dev:
                     dev_batch = to(dev_batch, device)
                     dev_loss_, n_dev_batch_ = model.get_loss(dev_batch)
                     dev_loss += dev_loss_*n_dev_batch_
                     n_dev_batch += n_dev_batch_
-                logger.info('[Dev] Step {0}, Loss: {1:.5f}'.format(step, dev_loss/n_dev_batch))
+                dev_loss /= n_dev_batch
+                
+                # dev summary
+                dev_metrics = {'loss':dev_loss}
+                dev_summ.write(step, dev_metrics)
+
+                logger.info('[Dev] Step {0}, Loss: {1:.5f}'.format(step, dev_loss))
+
                 dev_loss = 0
                 n_dev_batch = 0
+
+                checkpoint = dict(
+                        state_dict=model.state_dict(),
+                        optimizer_state=optimizer.state_dict(),
+                        step=step,
+                        best_valid_metric=best_valid_metric,
+                        best_test_metric=best_test_metric,
+                        best_step=best_step)
+                
+                cp_path = os.path.join(model_path, 'model.pkl')
+                torch.save(checkpoint, cp_path)
+                logger.info('[Save] Model saved to {0}'.format(cp_path))
+
+            if step % n_train == 0:
+                # save model for this epoch
+                cp_path = os.path.join(model_path, 'epoch_{0}.pkl'.format(epoch))
+                torch.save(checkpoint, cp_path)
+                logger.info('[Save] Model saved to {0}'.format(cp_path))
+               
+
 
                 # dev_loss_all += dev_loss
                 # print('dev loss', dev_loss)
---
+++ s2s/utils/summary.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import os
+
+class Summary:
+
+    def __init__(self, 
+            path, 
+            prefix, 
+            restart=False, 
+            xlabel='step',
+            columns=None,
+            sep='\t'):
+
+        self.path = path
+        self.prefix = prefix
+        self.restart = restart
+        self.xlabel = xlabel
+        self.columns = columns
+        self.sep = sep
+        
+        self.file = os.path.join(path, '{0}_summary.tsv').format(self.prefix)
+
+
+    def join(self, l):
+        return (self.sep).join(map(str, l)) + '\n'
+
+    
+    def write(self, xval, ys):
+        """
+        ys: dict
+        """
+        if self.restart:
+            if self.columns is None:
+                self.columns = list(ys.keys())
+            with open(self.file, 'w') as f:
+                f.write(self.join([self.xlabel] + self.columns))
+            self.restart = False
+        with open(self.file, 'a') as f:
+            yvals = [ys[col] for col in self.columns]
+            f.write(self.join([xval] + yvals))
+
+
+
+    
+
